61e628bcd933d458d318275e39433b6ba06028e7
PetraVidnerova <petra.vidnerova@gmail.com>
2016-04-13 12:10:47 +0200

    moved to function

--- hello_world.py
+++ hello_world.py
@@ -3,9 +3,12 @@ from scoop import futures
 def helloWorld(val):
     return "Hello world %s" % val
 
-
-if __name__ == "__main__":
+def test(): 
     ret_values = list(futures.map(helloWorld, range(16)))
     for x in ret_values:
         print(x)
 
+
+if __name__ == "__main__":
+
+    test() 
